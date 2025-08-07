3536e464a8d1af5199b26e672d52ec0b5ac939aa
Miniassasin101 <130169196+Miniassasin101@users.noreply.github.com>
2025-08-07 15:03:50 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"We are ready to go in, {bot.user.name}")
+
+@bot.command()
+async def black(ctx):
+    await ctx.send('Monkey!')
+
+@bot.command()
+async def sanchez(ctx):
+    await ctx.send('Fucking Weeb!')
+
+@bot.command()
+async def ginger(ctx):
+    await ctx.send('Ethan Baker the souless ginger!')
+
+@bot.command()
+async def unemployed(ctx):
+    await ctx.send('@luhkapalot')
+
+@bot.command()
+async def carter(ctx):
+    await ctx.send('I may be a Clanker, but at least im not a NIG-')
+
+@bot.command()
+async def image(ctx):
+    """Send a local image file."""
+    # adjust the path to wherever you stored it
+    file_path = "tepig.png"
+    # send it
+    await ctx.send(file=discord.File(file_path))
+
+@bot.command()
+async def jeremy(ctx):
+    """Send a local image file."""
+    # adjust the path to wherever you stored it
+    file_path = "jeremyc.png"
+    # send it
+    await ctx.send(file=discord.File(file_path))
+    await ctx.send('Look at this beautiful boy!')
+
+@bot.command()
+async def pingremy(ctx):
+    await ctx.send('<@418373528077991946>')
+
+@bot.command()
+async def pingbaker(ctx):
+    await ctx.send('<@379305399813144587>')
+
+@bot.command()
+async def echome(ctx, *arg):
+    arguments = ', '.join(arg)
+    await ctx.send(f'{len(arg)} arguments: {arguments}')
+
+@bot.command()
+async def add(ctx, arg1: int, arg2: int):
+    result = arg1 + arg2
+    if result == 19 and (arg1 == 9 or arg2 == 9):
+        result = 21
+        await ctx.send(f'{result} :)')
+        return
+
+    elif result == 420:
+        await ctx.send(f'{result} :)')
+        file_path = "weedimage.png"
+        # send it
+        await ctx.send(file=discord.File(file_path))
+        return
+
+
+    await ctx.send(f'{result}')
+
+@bot.command()
+async def commands(ctx):
+    await ctx.send('add: math command\nechome: Repeats back what you type after.\nblack\nsanchez\nginger\nunemployed\njeremy\npingbaker\npingremy\n')
+
+
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
